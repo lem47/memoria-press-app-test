3d6f0296d237928905acc15a566fdeb898442f84
Oleg Lementarev <lementarev@gmail.com>
2022-07-19 17:42:04 +0300

    students table was created

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import './App.scss';
 import { Header } from './Components/Header/Header';
 import { FilterBar } from './Components/FilterBar/FilterBar';
 import { SearchBar } from './Components/SearchBar/SearchBar';
+import { PeoplePage } from './Components/StudentsTable/PeoplePage/PeoplePage';
 
 export const App = () => (
   <div className="App">
     <Header />
     <FilterBar />
     <SearchBar />
+    <PeoplePage />
   </div>
 );
