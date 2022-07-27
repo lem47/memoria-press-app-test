8dc9d8e0e4d110f922e140939d367b51dcf45f06
Oleg Lementarev <lementarev@gmail.com>
2022-07-27 18:36:41 +0300

    inner info was created

--- src/Components/StudentsTable/PersonRow/PersonRow.jsx
+++ src/Components/StudentsTable/PersonRow/PersonRow.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable max-len */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './PersonRow.scss';
 import Checkbox from '../Checkbox/Checkbox';
+import { PersonResult } from '../PersonResult/PersonResult';
 
 export const PersonRow = ({
   person,
@@ -12,7 +14,10 @@ export const PersonRow = ({
 
   return (
     <>
-      <tr className="PersonRow" onClick={() => setIsExpand(!isExpand)}>
+      <tr
+        className={person.id % 2 === 0 ? 'PersonRow active' : 'PersonRow'}
+        onClick={() => setIsExpand(!isExpand)}
+      >
         <td className="PersonRow__check">
           <Checkbox
             key={person.id + 100}
@@ -47,7 +52,7 @@ export const PersonRow = ({
         </td>
         <td className="PersonRow__value">
           <div className="PersonRow__parents">
-            <div>
+            <div className="PersonRow__parents">
               <svg
                 width="14"
                 height="14"
@@ -56,19 +61,7 @@ export const PersonRow = ({
                 xmlns="http://www.w3.org/2000/svg"
                 className="PersonRow__info"
               >
-                <path
-                  d="M6.33325 10.3334H7.66659V6.33337H6.33325V10.3334ZM6.99992
-                  0.333374C3.31992 0.333374 0.333252 3.32004 0.333252
-                  7.00004C0.333252 10.68 3.31992 13.6667 6.99992 13.6667C10.6799
-                  13.6667 13.6666 10.68 13.6666 7.00004C13.6666 3.32004 10.6799
-                  0.333374 6.99992 0.333374ZM6.99992 12.3334C4.05992 12.3334
-                  1.66659 9.94004 1.66659 7.00004C1.66659 4.06004 4.05992
-                  1.66671 6.99992 1.66671C9.93992 1.66671 12.3333 4.06004
-                  12.3333 7.00004C12.3333 9.94004 9.93992 12.3334 6.99992
-                  12.3334ZM6.33325 5.00004H7
-                  .66659V3.66671H6.33325V5.00004Z"
-                  fill="#C0C0C0"
-                />
+                <path d="M6.33325 10.3334H7.66659V6.33337H6.33325V10.3334ZM6.99992 0.333374C3.31992 0.333374 0.333252 3.32004 0.333252 7.00004C0.333252 10.68 3.31992 13.6667 6.99992 13.6667C10.6799 13.6667 13.6666 10.68 13.6666 7.00004C13.6666 3.32004 10.6799 0.333374 6.99992 0.333374ZM6.99992 12.3334C4.05992 12.3334 1.66659 9.94004 1.66659 7.00004C1.66659 4.06004 4.05992 1.66671 6.99992 1.66671C9.93992 1.66671 12.3333 4.06004 12.3333 7.00004C12.3333 9.94004 9.93992 12.3334 6.99992 12.3334ZM6.33325 5.00004H7.66659V3.66671H6.33325V5.00004Z" fill="#C0C0C0" />
               </svg>
               {person.parents[0]}
               {', '}
@@ -82,13 +75,16 @@ export const PersonRow = ({
               xmlns="http://www.w3.org/2000/svg"
               className="PersonRow__arrow"
             >
-              <path d="M0 0L5 5L10 0H0Z" fill="#C0C0C0" />
+              {isExpand
+                ? (<path d="M0 5L5 0L10 5H0Z" fill="#5B5B5B" />)
+                : (<path d="M0 0L5 5L10 0H0Z" fill="#C0C0C0" />)
+              }
             </svg>
           </div>
         </td>
       </tr>
       <tr>
-        {isExpand && <p>EXPANDED</p>}
+        {isExpand && <PersonResult student={person} />}
       </tr>
     </>
   );
